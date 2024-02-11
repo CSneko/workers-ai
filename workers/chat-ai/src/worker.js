b513c9431e72ee44bcdd96ec82b91923b599aa20
CSneko <minecrafthetun@outlook.com>
2024-02-11 22:50:26 +0800

    更新workers

--- workers/chat-ai/src/worker.js
+++ workers/chat-ai/src/worker.js
@@ -3,6 +3,10 @@ import { Ai } from './vendor/@cloudflare/ai';
 
 var src_default = {
   async fetch(request, env) {
+  //判断请求
+  if (request.method == "OPTIONS"){
+    return createCORSResponse("{'status':'success'}",{ 'Content-Type': 'application/json' },200);
+  } 
   // 基本信息获取
   const url = new URL(request.url);
   const params = url.searchParams;
@@ -11,7 +15,11 @@ var src_default = {
   // 获取请求头部信息
   const headers = request.headers
   // 获取聊天记录的值
-  const past_msgs = headers.get('msg')
+  var past_msgs = headers.get('msg')
+  // 对消息进行解码
+  if(past_msgs != null){
+    past_msgs = decodeURIComponent(past_msgs);
+  }
   
   if (prompt !== null && prompt !== undefined) {
     prompt = prompt.replaceAll(`"`, "");
@@ -32,16 +40,16 @@ var src_default = {
     "prompt": "${prompt}"
   }`;
   
-  return new Response(requestJson, {
-    headers: { 'Content-Type': 'application/json' }
-  });
+  return await createCORSResponse(requestJson, 
+    { 'Content-Type': 'application/json' }
+  ,200);
 
   }
 };
 
 async function getRes(prompt, text, env, past_msgs) {
   try{
-  const API_KEY = "Your API Key";
+  const API_KEY = "AIzaSyDoZTP0mkKBIwFOrbG-shI_fLG6Mv6QwCQ";
   var msg_json = {
     contents: [{
       role: "user",
@@ -79,25 +87,6 @@ async function getRes(prompt, text, env, past_msgs) {
     body: JSON.stringify(post_body)  // 需要将对象转换为 JSON 字符串
   };
   
-  /*
-  {
-    "contents": [{
-      role: "model",
-        parts: [
-          {  text: `消息1` }
-        ]},
-      {role: "user",
-        parts: [
-          {  text: `消息2` }
-        ]},
-      {role: "model",
-        parts: [
-          {  text: `消息3` }
-        ]},
-    }]
-  } 
-   */
-  
   const response = await fetch(`https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`, requestOptions);
   
   if (!response.ok) {
@@ -107,7 +96,7 @@ async function getRes(prompt, text, env, past_msgs) {
   const data = await response.json();
   let reText = data.candidates[0].content.parts[0].text;
   // 替换掉不合法的字符
-  reText = reText.replaceAll(`"`, "");
+  reText = reText.replaceAll(`"`, `\\"`);
   reText = reText.replaceAll(/\n/g,"\\n");
   reText = reText.replaceAll("\\n\\n","\\n")
   return reText;
@@ -129,11 +118,31 @@ async function getRes(prompt, text, env, past_msgs) {
   });
   const tran_rep_json = JSON.parse(await Response.json(tran_rep).text()) //获取翻译后的文本JSON
   const tran_rep_text = tran_rep_json.translated_text
-  return tran_rep_text.replaceAll(`"`,"");
+  return tran_rep_text.replaceAll(`"`,`\\"`);
 }
   
 }
 export {
   src_default as default
 };
+
+async function createCORSResponse(value, headers, status) {
+  let response = new Response(value, { status });
+  response.headers.set("Access-Control-Allow-Origin", "*");
+  response.headers.set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,TRACE,DELETE,HEAD,OPTIONS");
+  response.headers.set("Access-Control-Max-Age", "86400");
+  response.headers.set("Access-Control-Allow-Headers", "*");
+  
+    // 添加 Content-Type 头部
+  if (!response.headers.has('Content-Type')) {
+      response.headers.set('Content-Type', 'application/json');
+  }
+  // 设置其他自定义头部
+  for (const [key, value] of Object.entries(headers)) {
+    response.headers.set(key, value);
+  }
+  
+  return response;
+}
+
 //# sourceMappingURL=index.js.map
